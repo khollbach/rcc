8e5d721c5d4fcfcb1e3f7721ccdce6b460c36591
Kevan Hollbach <khollbach@gmail.com>
2026-03-19 10:21:15 -0400

    Copy stdin to stdout

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{self, Read, Write};
+
+use anyhow::Result;
+
+fn main() -> Result<()> {
+    let mut c_code = String::new();
+    io::stdin().lock().read_to_string(&mut c_code)?;
+
+    io::stdout().write_all(c_code.as_bytes())?;
+    io::stdout().flush()?;
+
+    Ok(())
 }
